1f496429532196a3a5f1ad89e48473ab8beee53a
Ike Mattice <ikemattice@icloud.com>
2021-08-21 10:03:30 -0700

    Initial Commit

---
+++ roll-calculator/roll_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  roll_calculatorApp.swift
+//  roll-calculator
+//
+//  Created by Ike Mattice on 8/21/21.
+//
+
+import SwiftUI
+
+@main
+struct roll_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
